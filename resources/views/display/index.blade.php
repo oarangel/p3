a64db793a7cd5da6f11ee74a06bd2cabada422fb
Oscar Rangel <oarangel6@gmail.com>
2018-04-03 16:47:10 -0400

    Updated Vendor Packagesand Index Files

--- resources/views/display/index.blade.php
+++ resources/views/display/index.blade.php
@@ -3,11 +3,11 @@
 
 @push('head')
     {{-- Page specific CSS includes should be defined here; this .css file does not exist yet, but we can create it --}}
-    {{--}}<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
           rel='stylesheet' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
           crossorigin='anonymous' xmlns="http://www.w3.org/1999/html">
 
-    <link href='/css/p3.css' rel='stylesheet'>--}}
+    <link href='/css/p3.css' rel='stylesheet'>
 @endpush
 
 
@@ -23,18 +23,19 @@
         <div>
             <label for='totalAmt'>* Enter Bill Amount</label>
             <input type='text' name='totalAmt' id='totalAmt' value='{{ old('totalAmt') }}'>
-            @include('errors.error-field', ['field' => 'totalAmt'])
+            @include('modules.error-field', ['field' => 'totalAmt'])
         </div>
 
         <div>
             <label for='totalPer'>Enter Number of Persons:</label>
-            <input type='text' name='totalPer' id='totalPer' value='{{ old('totalPer') }}'>
-            @include('errors.error-field', ['field' => 'totalPer'])
+            <input type='text' name='totalPer' id='totalPer' placeholder='2' value='{{ old('totalPer') }}'>
+            @include('modules.error-field', ['field' => 'totalPer'])
 
 
             <p class='ex2'> Minimun number of persons is 2 and Maximun is 20.</p>
             <p class='ex2'> Enter integer numbers.</p>
         </div>
+        <div>
         <label> Select Tip Percentage </label>
 
 
@@ -49,7 +50,8 @@
         </div>
 
         <input type='submit' value='Calculate' class='btn btn-primary'>
-        @include('errors.error-form')
+
+        @include('modules.error-form')
     </form>
 
     @if(isset($total))
